f47b7895c2aa9efe23c9d3fc4f90b36a4eda59b5
Prajwal Sathawane <prajwalsathawane2003@gmail.com>
2025-07-14 17:39:11 +0530

    Replace Header.css with HeaderNext.css for improved styling and fixed header functionality

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect } from "react"
-import "./Header.css";
+import "./HeaderNext.css";
 import personaldata from "./personal.json";
 
 const Header = () => {
